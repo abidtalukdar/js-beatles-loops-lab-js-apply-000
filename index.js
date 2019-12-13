494608f6d52587fbf91bcc4bf7aa115aa6db336e
Abid Talukdar <abiditalukdar@gmail.com>
2019-12-13 23:47:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var arr =[] ;
+  var i ; 
+  for (i=0; i<musicians.length; i++) {
+    arr.push(musicians[i] + " plays " + instruments[i]) }
+  return arr ;
+}
+
+function johnLennonFacts(facts) {
+  var new_facts = [] ;
+  var i = 0 ;
+  while ( i < facts.length) {
+    new_facts.push( facts[i] + "!!!");
+    i++ ; }
+  return new_facts ; 
+}
+
+function iLoveTheBeatles(n) {
+  var empty_list = [] ;
+  n = 15 - n ;
+  do {
+    if (15 < n):
+      n = n - 1 ;
+      empty_list.push("I love the Beatles!") ;
+    else if   
+  } while (n !== 0 ) ;
+  return empty_list ;
+}  
